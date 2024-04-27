1df863c2ba792ea0ec8cf983fed2215857e2b59b
Wilbert Misingo <wilbertmisingo@gmail.com>
2024-04-27 20:41:37 +0300

    added model metadata

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -38,7 +38,7 @@ export const Home = () => {
     if (selectedImage) {
       setLoading(true);
 
-      const modelURL = "https://teachablemachine.withgoogle.com/models/AI2v_6cUB/"; 
+      const modelURL = "../../../models/"; 
       const classifier = await ml5.imageClassifier(modelURL);
       const image = document.createElement('img');
       image.src = URL.createObjectURL(selectedImage);
